d6b6bda19dbcdf8334bca95c81e176e7bb3dbf1d
jakob-huber <jakob.huber@eclipso.de>
2018-11-13 13:42:50 +0100

    INITIAL COMMIT

---
+++ loom.time.GTK/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Gtk;
+
+namespace loom.time.GTK
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            MainWindow win = new MainWindow();
+            win.Show();
+            Application.Run();
+        }
+    }
+}

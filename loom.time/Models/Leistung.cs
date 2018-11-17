3151d5d8b19a587da3ae61720ed0899bbf91fee4
Andreas_Hauber <Andreas_Hauber@gmx.de>
2018-11-17 02:12:17 +0100

    Grobe Struktur angelegt

---
+++ loom.time/Models/Leistung.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SQLite;
+
+namespace loom.time.Models
+{
+    public class Leistung
+    {
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int Id { get; set; }
+
+        [MaxLength(8)]
+        public string Symbol { get; set; }
+
+        public string Name { get; set; }
+    }
+}
+
---
+++ loom.time/Models/Vorgang.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SQLite;
+
+namespace loom.time.Models
+{
+    public class Vorgang
+    {
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int Id { get; set; }
+
+        [MaxLength(8)]
+        public string Symbol { get; set; }
+
+        public string Name { get; set; }
+    }
+}
+
+

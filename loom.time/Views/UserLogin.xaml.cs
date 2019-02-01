2e45d874386337876fd549cbb37e256b2abf05a4
jakob-huber <jakob.huber@eclipso.de>
2019-02-01 17:18:23 +0100

    Überschrift und Textfeld ergänzt

---
+++ loom.time/Views/UserLogin.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace loom.time
+{
+    public partial class UserLogin : ContentPage
+    {
+        public UserLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void PressMeButton_Pressed(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "You pressed me!";
+        }
+
+        private void PressMeButton_Clicked(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "I was just clicked!";
+        }
+
+    }
+
+
+    }
